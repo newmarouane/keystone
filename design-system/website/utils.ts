372f910c9a9df479cc1bf8899b176d1da3bbc9f6
newmarouane <80288727+newmarouane@users.noreply.github.com>
2026-08-08 00:41:15 +0100

    Delete design-system directory

--- design-system/website/utils.ts
+++
@@ -1,4 +0,0 @@
-export const toLabel = (str: string) => str.slice(0, 1).toUpperCase() + str.slice(1);
-
-const VOWELS = ['a', 'e', 'i', 'o', 'u'];
-export const aAn = (before: string) => `a${VOWELS.includes(before.charAt(0)) ? 'n' : ''}`;
